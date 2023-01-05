662c73e1d041536866c8e8bee218a8817c78d834
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-01-05 21:40:37 +0000

    Add files via upload
    learning how to test codes in python batch of function

---
+++ calcul_function.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    """add two numbers"""
+    return x+y
+
+def times(x,y):
+    """multiply two numbers"""
+    return x*y
+
+def power(x,y):
+    """ raise x to the power y"""
+
+if __name__ == '__main__':
+
+    x = add
+    print(x(3,6))
